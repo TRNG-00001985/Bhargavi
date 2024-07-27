5dd0d77933b3ed38f235fef02623abeaf67a5960
BhargaviGanta96 <141616439+BhargaviGanta96@users.noreply.github.com>
2024-07-27 16:25:24 +0530

    Add files via upload

---
+++ Stringconcat.java
@@ -0,0 +1,11 @@
+package revature;
+
+public class Stringconcat 
+{
+	public static void main(String[] args) {
+		String s1 = "Revature";
+		String s2 = " Training";
+		System.out.println(s1.concat(s2));
+		System.out.println(s2+s1);
+	}
+}
